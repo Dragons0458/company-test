e7d93092c0282d87443db4c21e3e9e05dde0b3d0
Juan Camilo Solano Estrada <juacase5826@outlook.com>
2021-09-10 16:14:51 -0700

    Initial commit, and it was added an initial "Fake database" base.

---
+++ src/fake-database/classes/database.ts
@@ -0,0 +1,89 @@
+import { AnimalNode } from 'src/fake-database/interfaces/animal-node';
+
+export class Database {
+  private data: AnimalNode = {
+    id: '1',
+    label: 'root',
+    children: [],
+  };
+
+  private indexedData = new Map<string, AnimalNode>([
+    [this.data.id, this.data],
+  ]);
+  private indexedParents = new Map<string, AnimalNode>([
+    [this.data.id, undefined],
+  ]);
+
+  private indexData(node: AnimalNode) {
+    this.indexedData.set(node.id, node);
+
+    for (const child of node.children) {
+      this.indexedParents.set(child.id, node);
+
+      this.indexData(child);
+    }
+  }
+
+  populateData(node: AnimalNode) {
+    this.data = node;
+
+    this.indexData(node);
+  }
+
+  getNode(nodeId: string): AnimalNode {
+    if (!this.indexedData.has(nodeId))
+      throw new Error("The node doesn't exists");
+
+    return this.indexedData.get(nodeId);
+  }
+
+  addNode(parentId: string, node: AnimalNode) {
+    if (!this.indexedData.has(parentId))
+      throw new Error("The parent node doesn't exists");
+
+    const parentNode = this.indexedData.get(parentId);
+
+    this.indexedParents.set(node.id, parentNode);
+    this.indexData(node);
+  }
+
+  updateParentNode(nodeId: string, newParentId: string) {
+    if (!this.indexedData.has(nodeId) || !this.indexedData.has(newParentId))
+      throw new Error("Some of the node ids don't exist");
+
+    const currParentNode = this.indexedParents.get(nodeId);
+    const currParentChildren = currParentNode.children;
+    let currIndexNodeInChildren: number;
+    let currNodeInChildren: AnimalNode;
+
+    for (let index = 0; index < currParentChildren.length; index++) {
+      const value = currParentChildren[index];
+
+      if (value.id === nodeId) {
+        currIndexNodeInChildren = index;
+        currNodeInChildren = value;
+        break;
+      }
+    }
+
+    currParentChildren.splice(currIndexNodeInChildren, 1);
+
+    const newParentNode = this.indexedParents.get(newParentId);
+
+    this.indexedData.get(newParentId).children.push(currNodeInChildren);
+    this.indexedParents.set(nodeId, newParentNode);
+  }
+
+  deleteNode(nodeId: string) {
+    if (!this.indexedParents.has(nodeId))
+      throw new Error("The node doesn't exists");
+
+    const parentChildren = this.indexedParents.get(nodeId).children;
+
+    const indexNodeInChildren = parentChildren.findIndex(
+      (value) => value.id === nodeId,
+    );
+
+    parentChildren.splice(indexNodeInChildren, 1);
+  }
+}
